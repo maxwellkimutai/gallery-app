d29900341c0828d8014466025edd68ba4adf1783
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-05 07:40:35 +0300

    add image model

--- gallery/models.py
+++ gallery/models.py
@@ -18,3 +18,30 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    categories = models.ManyToManyField(categories)
+    image_url = models.ImageField(upload_to = 'images/',blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        images = cls.objects.filter(categories__name__contains = search_term)
+        if len(images) < 1:
+            case_images = cls.objects.filter(categories__name__contains = search_term.capitalize())
+            return case_images
+        else:
+            return images
