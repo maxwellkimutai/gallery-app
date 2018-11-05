aa25b60062ae6c5e3850dd0c1222b2eb9d551629
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-05 07:49:03 +0300

    import django heroku

--- gallaryapp/settings.py
+++ gallaryapp/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
+
 from decouple import config,Csv
 
 MODE=config("MODE",default="dev")
