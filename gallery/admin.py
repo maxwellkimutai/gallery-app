56a01f5db308b1b26defdaf3d78812a3a25f8cdf
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-05 07:38:28 +0300

    register models in admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,8 +3,7 @@ from .models import Location, categories, Image
 
 # Register your models here.
 
-class ImageAdmin(admin.ModelAdmin):
-    filter_horizontal = ('categories',)
+
 
 admin.site.register(Location)
 admin.site.register(Image,ImageAdmin)
