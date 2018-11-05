a7289b130c0a74ded90b9958496aa9e5badb7c81
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-05 07:48:14 +0300

    add include gallery url

--- gallaryapp/urls.py
+++ gallaryapp/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'',include('gallery.urls'))
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
