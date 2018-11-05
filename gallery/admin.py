cefa897da8ea2ade4a468a4ec74231161cd60d9e
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-05 07:39:16 +0300

    add horizontal filter for image categories

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,7 +3,8 @@ from .models import Location, categories, Image
 
 # Register your models here.
 
-
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
 
 admin.site.register(Location)
 admin.site.register(Image,ImageAdmin)
