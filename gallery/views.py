8cc0d34d2f0db02b7f351519c299c855a6d3f7cb
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-05 07:44:01 +0300

    add location view

--- gallery/views.py
+++ gallery/views.py
@@ -13,3 +13,9 @@ def location(request,location):
     selected_location = Location.objects.get(id = location)
     images = Image.objects.filter(location = selected_location.id)
     return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
+
+def search(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+    return render(request,'search.html',{"images":searched_images,"category":search_term})
